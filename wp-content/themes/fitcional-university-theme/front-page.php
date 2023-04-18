f9659f163e9873b2529a9e52ac83309956335e84
diegogranados143@gmail.com <diego@codeando.edu.pe>
2023-04-18 16:12:15 -0400

    Fix query home events
    Order by event date and filter to be older than today's day

--- wp-content/themes/fitcional-university-theme/front-page.php
+++ wp-content/themes/fitcional-university-theme/front-page.php
@@ -17,11 +17,22 @@
             <div class="full-width-split__inner">
                 <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
                 <?php
+                $today = date('Ymd');
                 $homepageEvents = new WP_Query(array(
                     'posts_per_page' => 2,
-                    'post_type' => 'event'
+                    'post_type' => 'event',
+                    'meta_key' => 'event-date',
+                    'order_by' => 'meta_value_num',
+                    'order' => 'ASC',
+                    'meta_query' => array(
+                        array(
+                            'key' => 'event-date',
+                            'compare' => '>=',
+                            'value' => $today,
+                            'type' => 'numeric'
+                        )
+                    )
                 ));
-
                 while ($homepageEvents->have_posts()) {
                     $homepageEvents->the_post(); ?>
                     <div class="event-summary">
