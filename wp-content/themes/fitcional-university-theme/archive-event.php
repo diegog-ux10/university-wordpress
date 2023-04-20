10b20f3cc50c69ebc5565d05f0d7d2814c462be6
diegogranados143@gmail.com <diego@codeando.edu.pe>
2023-04-20 11:50:59 -0400

    Pagination and link to past events
    Pagination to events archive and added a link to see past events page

--- wp-content/themes/fitcional-university-theme/archive-event.php
+++ wp-content/themes/fitcional-university-theme/archive-event.php
@@ -16,8 +16,9 @@
             the_post(); ?>
             <div class="event-summary">
                 <a class="event-summary__date t-center" href="#">
-                    <span class="event-summary__month"><?php the_time('M') ?></span>
-                    <span class="event-summary__day"><?php the_time('d') ?></span>
+                <?php $eventDate = new DateTime(get_field('event-date'));?>
+                    <span class="event-summary__month"><?php echo $eventDate->format('M') ?></span>
+                    <span class="event-summary__day"><?php echo $eventDate->format('d') ?></span>
                 </a>
                 <div class="event-summary__content">
                     <h5 class="event-summary__title headline headline--tiny"><a
@@ -30,6 +31,8 @@
         endwhile;
         echo paginate_links();
         ?>
+        <hr class="section-break">
+        <p>Looking for a recap of past events? <a href="<?php echo site_url('/past-event') ?>">Check out past events</a></p>
     </div>
 
 <?php get_footer(); ?>
