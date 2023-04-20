172fb3d8ae197c399d6cc52b07c232536e29bfa1
diegogranados143@gmail.com <diego@codeando.edu.pe>
2023-04-20 11:49:59 -0400

    Added Program Post Type

--- wp-content/mu-plugins/university-posts-types.php
+++ wp-content/mu-plugins/university-posts-types.php
@@ -2,6 +2,7 @@
 
 function university_post_types(): void
 {
+    // Events Post Type
     register_post_type('event', array(
         'supports' => array('title', 'editor', 'excerpt'),
         'rewrite' => array('slug' => 'events'),
@@ -16,6 +17,25 @@ function university_post_types(): void
             'singular_name' => 'Event'
         ),
         'menu_icon' => 'dashicons-calendar'
+
+    ));
+
+    // Programs Post Type
+
+    register_post_type('program', array(
+        'supports' => array('title', 'editor', 'excerpt'),
+        'rewrite' => array('slug' => 'programs'),
+        'has_archive' => true,
+        'public' => true,
+        'show_in_rest' => true,
+        'labels' => array(
+            'name' => 'Programs',
+            'add_new_item' => 'Add New Program',
+            'edit_item' => 'Edit Program',
+            'all_items' => 'All Programs',
+            'singular_name' => 'Program'
+        ),
+        'menu_icon' => 'dashicons-awards'
 
     ));
 }
