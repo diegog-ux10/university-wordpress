c8bb89cf7c9a46d3475e9d9534c0e5c171fdbfca
diegogranados143@gmail.com <diego@codeando.edu.pe>
2023-04-22 07:55:46 -0400

    Format and comments

--- wp-content/themes/fitcional-university-theme/archive-program.php
+++ wp-content/themes/fitcional-university-theme/archive-program.php
@@ -1,14 +1,17 @@
+<!-- Site Header -->
 <?php get_header(); ?>
-    <div class="page-banner">
-        <div class="page-banner__bg-image"
-             style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
-        <div class="page-banner__content container container--narrow">
-            <h1 class="page-banner__title">All Programs</h1>
-            <div class="page-banner__intro">
-                <p>See what's going on in out world.</p>
-            </div>
+<!-- Page Banner -->
+<div class="page-banner">
+    <div class="page-banner__bg-image"
+            style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);">
+    </div>
+    <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title">All Programs</h1>
+        <div class="page-banner__intro">
+            <p>See what's going on in out world.</p>
         </div>
     </div>
+</div><!-- Page Banner End-->
 
     <div class="container container--narrow page-section">
         <ul class="link-list min-list">
