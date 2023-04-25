aa0d864bf64d8e82401b5fda118d3c129f1d6b55
diegogranados143@gmail.com <diego@codeando.edu.pe>
2023-04-25 12:03:33 -0400

    Added Template Parts Event and Blog Posts
    The template parts folder was created to store all the template parts.

--- wp-content/themes/fitcional-university-theme/front-page.php
+++ wp-content/themes/fitcional-university-theme/front-page.php
@@ -13,88 +13,92 @@
     </div>
 </div><!-- Page Banner End-->
 
-    <div class="full-width-split group">
-        <div class="full-width-split__one">
-            <div class="full-width-split__inner">
-                <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
-                <?php
-                $today = date('Ymd');
-                $homepageEvents = new WP_Query(array(
-                    'posts_per_page' => 2,
-                    'post_type' => 'event',
-                    'meta_key' => 'event-date',
-                    'order_by' => 'meta_value_num',
-                    'order' => 'ASC',
-                    'meta_query' => array(
-                        array(
-                            'key' => 'event-date',
-                            'compare' => '>=',
-                            'value' => $today,
-                            'type' => 'numeric'
-                        )
+<!-- Upcoming Events and Recent Blog Posts Section -->
+<div class="full-width-split group">
+    <!-- Events Column -->
+    <div class="full-width-split__one">
+        <div class="full-width-split__inner">
+            
+            <h2 class="headline headline--small-plus t-center">
+                Upcoming Events
+            </h2>
+            
+            <?php
+            /**Query to get:
+             * two last upcoming events
+             */
+            $today = date('Ymd');
+            $homepageEvents = new WP_Query(array(
+                'posts_per_page' => 2,
+                'post_type' => 'event',
+                'meta_key' => 'event-date',
+                'order_by' => 'meta_value_num',
+                'order' => 'ASC',
+                'meta_query' => array(
+                    array(
+                        'key' => 'event-date',
+                        'compare' => '>=',
+                        'value' => $today,
+                        'type' => 'numeric'
                     )
-                ));
-                while ($homepageEvents->have_posts()) {
-                    $homepageEvents->the_post(); ?>
-                    <div class="event-summary">
-                        <a class="event-summary__date t-center" href="#">
-                        <?php $eventDate = new DateTime(get_field('event-date'));?>
-                            <span class="event-summary__month"><?php echo $eventDate->format('M') ?></span>
-                            <span class="event-summary__day"><?php echo $eventDate->format('d') ?></span>
-                        </a>
-                        <div class="event-summary__content">
-                            <h5 class="event-summary__title headline headline--tiny"><a
-                                        href="<?php the_permalink() ?>"><?php the_title() ?></a></h5>
-                            <p><?php if (has_excerpt()):
-                                    echo get_the_excerpt();
-                                else:
-                                    echo wp_trim_words(get_the_content(), 18);
-                                endif;
-                                ?><a href="<?php the_permalink() ?>" class="nu gray">Learn more</a></p>
-                        </div>
-                    </div>
-                    <?php
-                }
-                wp_reset_postdata();
-                ?>
-                <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event'); ?>"
-                                                 class="btn btn--blue">View All Events</a></p>
-            </div>
+                )
+            ));
+            /**
+             * Loop Show Events
+             */
+            while ($homepageEvents->have_posts()):
+                $homepageEvents->the_post();
+                get_template_part('template-parts/content', get_post_type());
+            endwhile;
+            /**
+             * Reset Data Query
+             */
+            wp_reset_postdata();
+            ?>
+            <p class="t-center no-margin">
+                <a href="<?php echo get_post_type_archive_link('event'); ?>"
+                    class="btn btn--blue">
+                    View All Events
+                </a>
+            </p>
         </div>
-        <div class="full-width-split__two">
-            <div class="full-width-split__inner">
-                <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
-                <?php
-                $newPosts = new WP_Query(array(
-                    'posts_per_page' => 2,
-                ));
-                while ($newPosts->have_posts()):
-                    $newPosts->the_post(); ?>
-                    <div class="event-summary">
-                        <a class="event-summary__date event-summary__date--beige t-center" href="#">
-                            <span class="event-summary__month"><?php the_time('M') ?></span>
-                            <span class="event-summary__day"><?php the_time('d') ?></span>
-                        </a>
-                        <div class="event-summary__content">
-                            <h5 class="event-summary__title headline headline--tiny"><a
-                                        href="<?php the_permalink() ?>"><?php the_title(); ?></a></h5>
-                            <p><?php if (has_excerpt()):
-                                    echo get_the_excerpt();
-                                else:
-                                    echo wp_trim_words(get_the_content(), 18);
-                                endif;
-                                ?><a href="<?php the_permalink() ?>" class="nu gray">Learn more</a></p>
-                        </div>
-                    </div>
-                <?php
-                endwhile;
-                wp_reset_postdata();
-                ?>
-                <p class="t-center no-margin"><a href="<?php echo site_url('/blog') ?>" class="btn btn--yellow">View All
-                        Blog Posts</a></p>
-            </div>
+    </div><!-- Events Column End-->
+    <!-- Blog Posts Column -->
+    <div class="full-width-split__two">
+        <div class="full-width-split__inner">
+
+            <h2 class="headline headline--small-plus t-center">
+                From Our Blogs
+            </h2>
+            
+            <?php
+            /**Query to get:
+             * two more recent Blog Posts
+             */
+            $newPosts = new WP_Query(array(
+                'posts_per_page' => 2,
+            ));
+            /**
+             * Loop Show Blog Posts
+             */
+            while ($newPosts->have_posts()):
+                $newPosts->the_post();
+                get_template_part('template-parts/content', get_post_type());
+            endwhile;
+            /**
+             * Reset Data Query
+             */
+            wp_reset_postdata();
+            ?>
+            <p class="t-center no-margin">
+                <a href="<?php echo site_url('/blog') ?>" 
+                   class="btn btn--yellow">
+                   View All Blog Posts
+                </a>
+            </p>
         </div>
-    </div>
+    </div><!-- Blog Posts Column End-->
+</div><!-- Upcoming Events and Recent Blog Posts Section End-->
 
     <div class="hero-slider">
         <div data-glide-el="track" class="glide__track">
