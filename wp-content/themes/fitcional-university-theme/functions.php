7c49c7379ec06059429d191eb217aa396803424e
diegogranados143@gmail.com <diego@codeando.edu.pe>
2023-09-01 10:35:41 -0400

    wik

--- wp-content/themes/fitcional-university-theme/functions.php
+++ wp-content/themes/fitcional-university-theme/functions.php
@@ -1,5 +1,18 @@
 <?php
 
+function university_custom_rest() {
+  register_rest_field("post", "authorName", array(
+    'get_callback' => function() {
+      return get_the_author();
+    },
+    
+  ));
+
+
+}
+
+add_action("rest_api_init", "university_custom_rest");
+
 // Site's Scripts and Stylesheet's:
 
 function university_files(): void
