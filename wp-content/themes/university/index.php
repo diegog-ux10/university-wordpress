8bc0538d75fbbfa8e4a2713554d840b368fde5c8
diegogranados143@gmail.com <diego@codeando.edu.pe>
2024-06-07 07:21:57 -0400

    index files created

--- wp-content/themes/university/index.php
+++ wp-content/themes/university/index.php
@@ -1,1 +1,13 @@
-New University Theme
+<?php get_header();
+
+while(have_posts()) {
+    the_post(); ?>
+    <a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
+    <p><?php the_content(); ?></p>
+    <hr>
+    
+<?php } 
+
+get_footer()
+
+?>
